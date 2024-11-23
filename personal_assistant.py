af6ac2de72026aad15a291a9bab534e554496403
Danilka06 <main@0x1f44.ru>
2024-11-23 13:05:41 +0300

    [add] classes template

--- personal_assistant.py
+++ personal_assistant.py
@@ -2,6 +2,32 @@ import csv
 import pandas as pd
 
 
+class Note:
+    def __init__(self):
+        pass
+
+
+class Task:
+    def __init__(self):
+        pass
+
+
+class Contact:
+    def __init__(self):
+        pass
+
+
+class FinanceRecord:
+    def __init__(self):
+        pass
+
+
+class Calculator:
+    def __init__(self):
+        pass
+
+
+
 def main():
     print("""Добро пожаловать в Персональный помощник!
 Выберите действие:
@@ -12,6 +38,7 @@ def main():
 5. Калькулятор
 6. Выход""")
     choice = int(input("Выберите действие:"))
+
 
 
 if __name__ == "__main__":
