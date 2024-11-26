91e123d74efa98a1f519424a478ada5fb398d79a
Danilka06 <main@0x1f44.ru>
2024-11-26 07:37:52 +0300

    [add] Note.create_note() done

--- src/note.py
+++ src/note.py
@@ -1,5 +1,7 @@
 import messages
 
+from datetime import datetime
+
 
 class Note:
     def __init__(self):
@@ -12,8 +14,8 @@ class Note:
             6: self.import_csv,
             7: self.export_csv,
         }
-        
-        self.run()
+
+        self.notes = {}
 
     def run(self):
         choice = 0
@@ -22,23 +24,35 @@ class Note:
             choice = input(messages.choose_message)
             func = self.func_choice[choice]
 
-    def create_note():
+    def create_note(self):
+        id = int(input("Введите id новой заметки"))
+        title = input(f"Введите title новой заметки")
+        content = input(f"Введите content новой заметки")
+        timestamp = datetime.now()
+
+        self.notes[id] = {
+            "id": id,
+            "title": title,
+            "content": content,
+            "timestamp": timestamp,
+        }
+
+    def list_notes(self):
         pass
 
-    def list_notes():
+    def list_note_details(self):
         pass
 
-    def list_note_details():
+    def redact_note(self):
         pass
 
-    def redact_note():
+    def delete_note(self):
         pass
 
-    def delete_note():
+    def import_csv(self):
+        # get from the file
         pass
 
-    def import_csv():
-        pass
-    
-    def export_csv():
+    def export_csv(self):
+        # load to the file
         pass
